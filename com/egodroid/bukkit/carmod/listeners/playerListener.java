25fe21716627158c5a8f661824c45c171a5921c1
Branden Boegh <boeghb@uwosh.edu>
2012-06-15 13:12:48 -0500

    Updated to new method for block checking for initial Car placement.

--- com/egodroid/bukkit/carmod/listeners/playerListener.java
+++ com/egodroid/bukkit/carmod/listeners/playerListener.java
@@ -25,62 +25,39 @@ public class playerListener implements Listener {
 		Block block = event.getClickedBlock();
 		
 		if(event.hasBlock() && this.mPlugin.getConfig().getBoolean("placeMinecart")) {
-			if (!this.mPlugin.getConfig().getBoolean("useWool")) {
-			
-				if (p.getItemInHand().getTypeId() == 328) {
-					if (block.getTypeId() == this.mPlugin.getConfig().getInt("streetBlock") || block.getTypeId() == this.mPlugin.getConfig().getInt("motorwayBlock")) {
-						if(!CarMod.permission.has(p, "minecars.create")){
-							p.sendMessage(ChatColor.DARK_GREEN+"[MineCars]"+ ChatColor.WHITE+" You don't have permission to place MineCars!");
-							
-							return;
-						}
-						Location temploc = event.getClickedBlock().getLocation();
-						temploc.add(new Vector(0,1,0));
-						Minecart m = p.getWorld().spawn(temploc, Minecart.class);
-						p.getInventory().removeItem(event.getPlayer().getInventory().getItemInHand());
-						mML.mineCars.add(m.getUniqueId());
-						mML.owners.put(p.getName(), m.getUniqueId());
-					}
+			if(mML.drivableBlock(block)&&p.getItemInHand().getTypeId() == 328){
+				if(!CarMod.permission.has(p, "minecars.create")){
+					p.sendMessage(ChatColor.DARK_GREEN+"[MineCars]"+ ChatColor.WHITE+" You don't have permission to place MineCars!");
+					
+					return;
 				}
-		
-			}else{
-				if (p.getItemInHand().getTypeId() == 328) {
-					if (block.getTypeId() == 35) {
-						if(!CarMod.permission.has(p, "minecars.create")){
-							p.sendMessage(ChatColor.DARK_GREEN+"[MineCars]"+ ChatColor.WHITE+" You don't have permission to place MineCars!");
-							
-							return;
-						}
-						Location temploc = block.getLocation();
-						temploc.add(new Vector(0,1,0));
-						Minecart m = p.getWorld().spawn(temploc, Minecart.class);
-						p.getInventory().removeItem(event.getPlayer().getInventory().getItemInHand());
-						mML.mineCars.add(m.getUniqueId());
-						mML.owners.put(p.getName(), m.getUniqueId());
-					}
-				}
+				Location temploc = event.getClickedBlock().getLocation();
+				temploc.add(new Vector(0,1,0));
+				Minecart m = p.getWorld().spawn(temploc, Minecart.class);
+				p.getInventory().removeItem(event.getPlayer().getInventory().getItemInHand());
+				mML.mineCars.add(m.getUniqueId());
+				mML.owners.put(p.getName(), m.getUniqueId());
 			}
+		}
 			
-			if (p.getItemInHand().getTypeId() == 328) {
-				if (block.getTypeId() == 43 || block.getTypeId() == 44) {
-					Step step = new Step(block.getType(), block.getData());
-					if (step.getData() == (byte) this.mPlugin.getConfig().getInt("StreetStepType")) { 
-						if(!CarMod.permission.has(p, "minecars.create")){
-							p.sendMessage(ChatColor.DARK_GREEN+"[MineCars]"+ ChatColor.WHITE+" You don't have permission to place MineCars!");
+		if (p.getItemInHand().getTypeId() == 328) {
+			if (block.getTypeId() == 43 || block.getTypeId() == 44) {
+				Step step = new Step(block.getType(), block.getData());
+				if (step.getData() == (byte) this.mPlugin.getConfig().getInt("StreetStepType")) { 
+					if(!CarMod.permission.has(p, "minecars.create")){
+						p.sendMessage(ChatColor.DARK_GREEN+"[MineCars]"+ ChatColor.WHITE+" You don't have permission to place MineCars!");
 							
-							return;
-						}
-						Location temploc = event.getClickedBlock().getLocation();
-						temploc.add(new Vector(0,1,0));
-						Minecart m = p.getWorld().spawn(temploc, Minecart.class);
-						p.getInventory().removeItem(event.getPlayer().getInventory().getItemInHand());
-						mML.mineCars.add(m.getUniqueId());
-						mML.owners.put(p.getName(), m.getUniqueId());
+						return;
 					}
+					Location temploc = event.getClickedBlock().getLocation();
+					temploc.add(new Vector(0,1,0));
+					Minecart m = p.getWorld().spawn(temploc, Minecart.class);
+					p.getInventory().removeItem(event.getPlayer().getInventory().getItemInHand());
+					mML.mineCars.add(m.getUniqueId());
+					mML.owners.put(p.getName(), m.getUniqueId());
 				}
 			}
 		}
-		
 	}
 	
 	public playerListener(CarMod pPlugin, minecartListener pML) {
